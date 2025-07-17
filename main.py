a42a154ca0c427d1ee65522aa183a2b5b544d6c7
tim-hardie <timothy.g.hardie@gmail.com>
2025-07-17 12:03:55 +0100

    add function to sort list of characters, and display output nicely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import sort_char_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,11 +9,28 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
+    file_path = "books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    
     # print number of words in frankenstein
-    #num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    #print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    num_words = get_num_words(file_contents)
+    print(f"Found {num_words} total words")
     
     # print number of characters in frankenstein
-    print(get_num_chars(get_book_text("books/frankenstein.txt")))
+    #num_chars = get_num_chars(file_contents)
+    #print(num_chars)
+
+    # sort dict of characters
+    print("--------- Character Count -------")
+    char_list_sorted = sort_char_dict(get_num_chars(file_contents))
+    for i in range(0,len(char_list_sorted)):
+        if char_list_sorted[i]["char"].isalpha():
+            print(f"{char_list_sorted[i]["char"]}: {char_list_sorted[i]["num"]}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,15 @@ def get_num_chars(book_text):
             char_dict[c.lower()] = 1
     
     return char_dict
+
+def sort_char_dict(char_dict):
+    sorted_char_list = []
+    for char in char_dict:
+        num = char_dict[char]
+        sorted_char_list.append({"char": char, "num": num})
+
+    def sort_on(items):
+        return items["num"]
+    sorted_char_list.sort(reverse=True, key=sort_on)
+    
+    return sorted_char_list
